cbaa037d8ec0c560cc7e4c60e6b751bab43ad6c3
Zachary C. Laborde <Zachary.Laborde@ibm.com>
2020-08-20 17:51:47 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './index.css'
+// import {ParallaxProvider, Parallax} from "react-scroll-parallax";
+
+// const ParallaxImage = () => (
+//   <Parallax className="custom-class" y={[-70, 70]} styleOuter={{zIndex: 2}} styleInner={{zIndex: 1}}>
+//     <img src={MyImage}/>
+//   </Parallax>
+// );
+
+// const App = () => {
+//   return (
+//     <>
+//     <ParallaxProvider style = {{zIndex: 1}}>
+//       <div className={'testContainer'}>
+//         <ParallaxImage/>
+//       </div>
+//     </ParallaxProvider>
+//     <div className='container'><img src={MyImage}/></div>
+//       </>
+//   );
+// };
+
+const App = () => (<>
+  <header className='container main'>
+    <div className='blur'>
+      <h1>
+        My Name is Zach
+      </h1>
+      <h3>
+        blah blah
+      </h3>
+    </div>
+  </header>
+  <article className='container second'>
+    <div className='blur'>
+      <h2>
+        abc
+      </h2>
+    </div>
+  </article>
+</>);
+
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MyImage from './assets/header.jpg';
+import App from "./App";
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
